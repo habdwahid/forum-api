1c8bcfeee4598cb8816ec7e78f10bcb775e0efda
Hasbi <habdwaheed@gmail.com>
2023-08-29 15:38:55 +0700

    test(forum api): remove toBeInstanceOf from test

--- src/Commons/exceptions/_test/ClientError.test.js
+++ src/Commons/exceptions/_test/ClientError.test.js
@@ -3,6 +3,5 @@ const ClientError = require('../ClientError')
 describe('ClientError', () => {
   it('should throw error when directly use it', () => {
     expect(() => new ClientError('')).toThrowError('cannot instantiate abstract class')
-    expect(new ClientError()).toBeInstanceOf(Error)
   })
 })
