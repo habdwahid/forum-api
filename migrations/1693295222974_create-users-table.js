48b48f8c3df6d0670a74b19692ad8d8086493039
Hasbi <habdwaheed@gmail.com>
2023-08-29 14:54:27 +0700

    users table migration generated

---
+++ migrations/1693295222974_create-users-table.js
@@ -0,0 +1,28 @@
+/* eslint-disable camelcase */
+
+exports.up = (pgm) => {
+  pgm.createTable('users', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    fullname: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    username: {
+      type: 'VARCHAR(50)',
+      unique: true,
+    },
+    password: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    created_at: 'TIMESTAMP',
+    updated_at: 'TIMESTAMP',
+  });
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('users');
+};
