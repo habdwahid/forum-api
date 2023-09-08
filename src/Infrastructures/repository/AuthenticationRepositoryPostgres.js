e56827baf8b6c064611c65491816abccbada0562
Hasbi <habdwaheed@gmail.com>
2023-09-09 04:36:36 +0700

    feat(forum api): add AuthenticationRepositoryPostgres

---
+++ src/Infrastructures/repository/AuthenticationRepositoryPostgres.js
@@ -0,0 +1,41 @@
+const AuthRepository = require('../../Domains/authentications/AuthRepository')
+const InvariantError = require('../../Commons/exceptions/InvariantError')
+
+class AuthenticationRepositoryPostgres extends AuthRepository {
+  constructor(pool) {
+    super()
+
+    this._pool = pool
+  }
+
+  async addToken(token) {
+    const query = {
+      text: 'INSERT INTO authentications VALUES($1)',
+      values: [token]
+    }
+
+    await this._pool.query(query)
+  }
+
+  async checkAvailabilityToken(token) {
+    const query = {
+      text: 'SELECT token FROM authentications WHERE token = $1',
+      values: [token]
+    }
+
+    const result = await this._pool.query(query)
+
+    if (!result.rowCount) throw new InvariantError('refresh token tidak ditemukan di database')
+  }
+
+  async deleteToken(token) {
+    const query = {
+      text: 'DELETE FROM authentications WHERE token = $1',
+      values: [token]
+    }
+
+    await this._pool.query(query)
+  }
+}
+
+module.exports = AuthenticationRepositoryPostgres
