75f0f7dcc42e6ca374077486add7c51f80cb4cc5
Hasbi <habdwaheed@gmail.com>
2023-09-09 03:15:50 +0700

    feat(forum api): add LogoutUserUseCase

---
+++ src/Applications/use_case/LogoutUserUseCase.js
@@ -0,0 +1,23 @@
+class LogoutUserUseCase {
+  constructor({authenticationRepository}) {
+    this._authenticationRepository = authenticationRepository
+  }
+
+  async execute(useCasePayload) {
+    // Validating payload
+    this._validatePayload(useCasePayload)
+
+    const {refreshToken} = useCasePayload
+
+    await this._authenticationRepository.checkAvailabilityToken(refreshToken)
+    await this._authenticationRepository.deleteToken(refreshToken)
+  }
+
+  _validatePayload({refreshToken}) {
+    if (!refreshToken) throw new Error('DELETE_AUTHENTICATION_USE_CASE.NOT_CONTAIN_REFRESH_TOKEN')
+
+    if (typeof refreshToken !== 'string') throw new Error('DELETE_AUTHENTICATION_USE_CASE.NOT_MEET_DATA_TYPE_SPECIFICATION')
+  }
+}
+
+module.exports = LogoutUserUseCase
