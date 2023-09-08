c870470655464f6dc8268b0536333ca81f462baf
Hasbi <habdwaheed@gmail.com>
2023-09-09 00:46:24 +0700

    test(forum api): add comparePassword function test into PasswordHash test

--- src/Applications/security/_test/PasswordHash.test.js
+++ src/Applications/security/_test/PasswordHash.test.js
@@ -7,5 +7,6 @@ describe('PasswordHash interface', () => {
 
     // Action and assert
     await expect(passwordHash.hash('')).rejects.toThrowError('PASSWORD_HASH.METHOD_NOT_IMPLEMENTED')
+    await expect(passwordHash.comparePassword('plain', 'encrypted')).rejects.toThrowError('PASSWORD_HASH.METHOD_NOT_IMPLEMENTED')
   })
 })
