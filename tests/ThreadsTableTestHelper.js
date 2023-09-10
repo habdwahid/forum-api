7c1c9372211eaa174562f4883a2fe5fe7b5175ea
Hasbi <habdwaheed@gmail.com>
2023-09-10 17:28:02 +0700

    feat(forum api): add ThreadsTableTestHelper

---
+++ tests/ThreadsTableTestHelper.js
@@ -0,0 +1,31 @@
+/* istanbul ignore file */
+
+const pool = require('../src/Infrastructures/database/postgres/pool')
+
+const ThreadsTableTestHelper = {
+  async addThread({id = 'thread-123', title = 'Sebuah Thread', body = 'Sebuah body thread', owner = 'user-123'}) {
+    const createdAt = new Date().toISOString()
+    const updatedAt = createdAt
+    const query = {
+      text: 'INSERT INTO threads VALUES($1, $2, $3, $4, $5, $6)',
+      values: [id, title, body, owner, createdAt, updatedAt]
+    }
+
+    await pool.query(query)
+  },
+  async findThread(id) {
+    const query = {
+      text: 'SELECT * FROM threads WHERE id = $1',
+      values: [id]
+    }
+
+    const result = await pool.query(query)
+
+    return result.rows
+  },
+  async cleanTable() {
+    await pool.query('TRUNCATE TABLE threads')
+  }
+}
+
+module.exports = ThreadsTableTestHelper
