cca245281f3c4423e11c6697688461fab9507449
Hasbi <habdwaheed@gmail.com>
2023-09-11 21:57:11 +0700

    test(forum api): add ThreadCommentRepository test

---
+++ src/Domains/thread_comments/_test/ThreadCommentRepository.test.js
@@ -0,0 +1,13 @@
+const ThreadCommentRepository = require('../ThreadCommentRepository')
+
+describe('ThreadCommentRepository interface', () => {
+  it('should throw error when invoke abstract behavior', async () => {
+    // Arrange
+    const threadCommentRepository = new ThreadCommentRepository()
+
+    // Action and assert
+    await expect(threadCommentRepository.addComment({})).rejects.toThrowError('THREAD_COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED')
+    await expect(threadCommentRepository.verifyCommentOwner('')).rejects.toThrowError('THREAD_COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED')
+    await expect(threadCommentRepository.deleteComment('')).rejects.toThrowError('THREAD_COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED')
+  })
+})
