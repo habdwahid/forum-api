1b63a1916628cc72cdfa73184635694a6aa1f350
Hasbi <habdwaheed@gmail.com>
2023-09-11 21:57:29 +0700

    feat(forum api): add ThreadCommentRepository interface

---
+++ src/Domains/thread_comments/ThreadCommentRepository.js
@@ -0,0 +1,15 @@
+class ThreadCommentRepository {
+  async addComment(addComment) {
+    throw new Error('THREAD_COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED')
+  }
+
+  async verifyCommentOwner(id) {
+    throw new Error('THREAD_COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED')
+  }
+
+  async deleteComment(id) {
+    throw new Error('THREAD_COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED')
+  }
+}
+
+module.exports = ThreadCommentRepository
