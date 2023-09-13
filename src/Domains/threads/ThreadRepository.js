013b014fdae1c28d976871df726d56375f1ca8d8
Hasbi <habdwaheed@gmail.com>
2023-09-14 02:40:04 +0700

    feat(forum api): add findThreadById function

--- src/Domains/threads/ThreadRepository.js
+++ src/Domains/threads/ThreadRepository.js
@@ -2,6 +2,10 @@ class ThreadRepository {
   async addThread(userId, addThread) {
     throw new Error('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED')
   }
+
+  async findThreadById(id) {
+    throw new Error('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED')
+  }
 }
 
 module.exports = ThreadRepository
