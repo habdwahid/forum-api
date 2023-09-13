bb547077631585162514d7f0ab8fff54e3abf0b7
Hasbi <habdwaheed@gmail.com>
2023-09-14 04:34:09 +0700

    feat(forum api): add add thread translation message

--- src/Commons/exceptions/_test/DomainErrorTranslator.test.js
+++ src/Commons/exceptions/_test/DomainErrorTranslator.test.js
@@ -38,5 +38,11 @@ describe('DomainErrorTranslator', () => {
       .toStrictEqual(new InvariantError('tidak dapat membuat thread baru karena tipe data tidak sesuai'))
     expect(DomainErrorTranslator.translate(new Error('ADD_THREAD.TITLE_LIMIT_CHAR')))
       .toStrictEqual(new InvariantError('tidak dapat membuat thread baru karena karakter title melebihi batas limit'))
+
+    // ADD_COMMENT
+    expect(DomainErrorTranslator.translate(new Error('ADD_COMMENT.NOT_CONTAIN_NEEDED_PROPERTY')))
+      .toStrictEqual(new InvariantError('tidak dapat membuat komentar baru karena properti yang dibutuhkan tidak ada'))
+    expect(DomainErrorTranslator.translate(new Error('ADD_COMMENT.NOT_MEET_DATA_TYPE_SPECIFICATION')))
+      .toStrictEqual(new InvariantError('tidak dapat membuat komentar baru karena tipe data tidak sesuai'))
   })
 })
