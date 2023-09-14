52470cf7de6cd30b799a22f9339a0bc512e59bce
Hasbi <habdwaheed@gmail.com>
2023-09-14 16:59:57 +0700

    fix(forum api): change DeleteCommentUseCase parameter

--- src/Applications/use_case/DeleteCommentUseCase.js
+++ src/Applications/use_case/DeleteCommentUseCase.js
@@ -6,31 +6,18 @@ class DeleteCommentUseCase {
   /**
    * Executing the delete comment action.
    *
-   * @param {string} userId
-   * @param {object} useCasePayload
+   * @param {string} userId the user id.
+   * @param {string} commentId the comment id.
    */
-  async execute(userId, useCasePayload) {
-    // Validating payload.
-    this._validatePayload(useCasePayload)
-
-    const {id} = useCasePayload
+  async execute(userId, commentId) {
+    // Find comment by id
+    await this._threadCommentRepository.findCommentById(commentId)
 
     // Validating comment owner
-    await this._threadCommentRepository.verifyCommentOwner(userId, id)
+    await this._threadCommentRepository.verifyCommentOwner(userId, commentId)
 
     // Deleting comment
-    await this._threadCommentRepository.deleteComment(id)
-  }
-
-  /**
-   * Validate the payload.
-   *
-   * @param {string} id
-   */
-  _validatePayload({id}) {
-    if (!id) throw new Error('DELETE_COMMENT_USE_CASE.NOT_CONTAIN_NEEDED_PROPERTY')
-
-    if (typeof id !== 'string') throw new Error('DELETE_COMMENT_USE_CASE.NOT_MEET_DATA_TYPE_SPECIFICATION')
+    await this._threadCommentRepository.deleteComment(commentId)
   }
 }
 
--- src/Applications/use_case/_test/DeleteCommentUseCase.test.js
+++ src/Applications/use_case/_test/DeleteCommentUseCase.test.js
@@ -2,39 +2,17 @@ const DeleteCommentUseCase = require('../DeleteCommentUseCase')
 const ThreadCommentRepository = require('../../../Domains/thread_comments/ThreadCommentRepository')
 
 describe('DeleteCommentUseCase', () => {
-  it('should throw error when payload did not contain needed property', async () => {
-    // Arrange
-    const userId = 'user-123'
-    const useCasePayload = {}
-    const deleteCommentUseCase = new DeleteCommentUseCase({})
-
-    // Action and assert
-    await expect(deleteCommentUseCase.execute(userId, useCasePayload)).rejects.toThrowError('DELETE_COMMENT_USE_CASE.NOT_CONTAIN_NEEDED_PROPERTY')
-  })
-
-  it('should throw error when payload did not contain needed property', async () => {
-    // Arrange
-    const userId = 'user-123'
-    const useCasePayload = {
-      id: 12345
-    }
-    const deleteCommentUseCase = new DeleteCommentUseCase({})
-
-    // Action and assert
-    await expect(deleteCommentUseCase.execute(userId, useCasePayload)).rejects.toThrowError('DELETE_COMMENT_USE_CASE.NOT_MEET_DATA_TYPE_SPECIFICATION')
-  })
-
   it('should orchestrating the delete comment action', async () => {
     // Arrange
     const userId = 'user-123'
-    const useCasePayload = {
-      id: 'comment-123'
-    }
+    const commentId = 'comment-123'
 
     /* creating dependency of the use case */
     const mockThreadCommentRepository = new ThreadCommentRepository()
 
     /* mocking needed function */
+    mockThreadCommentRepository.findCommentById = jest.fn()
+      .mockImplementation(() => Promise.resolve())
     mockThreadCommentRepository.verifyCommentOwner = jest.fn()
       .mockImplementation(() => Promise.resolve())
     mockThreadCommentRepository.deleteComment = jest.fn()
@@ -46,10 +24,11 @@ describe('DeleteCommentUseCase', () => {
     })
 
     // Action
-    await getCommentUseCase.execute(userId, useCasePayload)
+    await getCommentUseCase.execute(userId, commentId)
 
     // Assert
-    expect(mockThreadCommentRepository.verifyCommentOwner).toHaveBeenCalledWith(userId, useCasePayload.id)
-    expect(mockThreadCommentRepository.deleteComment).toHaveBeenCalledWith(useCasePayload.id)
+    expect(mockThreadCommentRepository.findCommentById).toHaveBeenCalledWith(commentId)
+    expect(mockThreadCommentRepository.verifyCommentOwner).toHaveBeenCalledWith(userId, commentId)
+    expect(mockThreadCommentRepository.deleteComment).toHaveBeenCalledWith(commentId)
   })
 })
